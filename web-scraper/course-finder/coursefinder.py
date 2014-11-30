81fd245b116c617fe00db5b46d9284f5d40055d5
Qasim Iqbal <me@qas.im>
2014-11-30 13:41:29 -0500

    Ensure we get proper data back (dangerous)

--- web-scraper/course-finder/coursefinder.py
+++ web-scraper/course-finder/coursefinder.py
@@ -18,18 +18,24 @@ class CourseFinder:
     def update_cache(self):
         """Update the locally stored course pages."""
 
-        info = self.search('csc207')
+        info = self.search('')
 
         for x in info:
             course_id = re.search('offImg(.*)', x[0]).group(1)[:14]
             url = '%s/coursedetails/%s' % (self.host, course_id)
-            course_dir = '.cache/%s' % course_id[-5:]
+            course_dir = 'cache/%s' % course_id[-5:]
             os.makedirs(course_dir, exist_ok=True)
 
             r = self.s.get(url, cookies=self.cookies)
 
+            html = None
+            while html is None:
+                r = self.s.get(url, cookies=self.cookies)
+                if r.status_code == 200:
+                    html = r.text
+
             f = open('%s/%s.html' % (course_dir, course_id), 'wb')
-            f.write(r.text.encode('utf-8'))
+            f.write(html.encode('utf-8'))
             f.close()
 
     def search(self, query='', requirements=''):
