798bb865507716c6ccd3f970e99363128038df6c
Qasim Iqbal <me@qas.im>
2014-12-07 13:57:59 -0500

    Fixed tabbing

--- routes/courses.js
+++ routes/courses.js
@@ -1,33 +1,34 @@
-var express = require('express');
-var router = express.Router();
-var QUERIES = ["_id", "department", "division", "campus", "term", "section",
-"term", "postrequisite", "tutorials", "breadth", "time", "instructor",
-"location", "size", "rating"
+var express = require('express')
+var router = express.Router()
+
+var QUERIES = [
+  "_id", "department", "division", "campus", "term", "section",
+  "term", "postrequisite", "tutorials", "breadth", "time", "instructor",
+  "location", "size", "rating"
 ]
+
 router.get('/', function(req, res) {
 
- //Check for valid query
- var query = req.query
- console.log(query)
- var clean = true;
- var keys = [];
- for (var key in query) {
-  if (QUERIES.indexOf(key.toLowerCase()) < 0) {
-   clean = false;
-  }
-  keys.push(key);
-  console.log(key)
- }
- if (!clean)
-  res.send(400)
-  else {
-   for (var i = 0; i < keys.length; i++) {
-    var key = keys[i].toLowerCase()
-    console.log(key)
-    var quer = query[key]
-    if
-   }
+  var query = req.query
+  var clean = true
+  var keys = []
+
+  for (var key in query) {
+    if (QUERIES.indexOf(key.toLowerCase()) < 0) {
+      clean = false
+    }
+    keys.push(key)
   }
 
- });
- module.exports = router;
+  if (!clean) {
+    res.send(400)
+  } else {
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i].toLowerCase()
+      var quer = query[key]
+    }
+  }
+
+})
+
+module.exports = router
