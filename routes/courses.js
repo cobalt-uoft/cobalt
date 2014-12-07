78cff744b118655edad6bc372339463f2b8bc271
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2014-12-06 21:04:29 -0500

    Back end started

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
+var QUERIES = ["_id", "department", "division", "campus", "term", "section",
+  "term", "postrequisite", "tutorials", "breadth", "time", "instructor",
+  "location", "size", "rating"
+]
 router.get('/', function(req, res) {
-  res.json({"so much": "data boyz"});
-});
 
+  //Check for valid query
+  var query = req.query
+  console.log(query)
+  var clean = true;
+  var keys = [];
+  for (var key in query) {
+    if (QUERIES.indexOf(key.toLowerCase()) < 0) {
+      clean = false;
+    }
+    keys.push(key);
+    console.log(key)
+  }
+  if (!clean)
+    res.send(400)
+  else {
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i].toLowerCase()
+      console.log(key)
+      var quer = query[key]
+      if
+    }
+  }
+
+});
 module.exports = router;
