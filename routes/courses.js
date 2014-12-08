be69900d3c5188e3d4a3e32bb1a53ed927bda299
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2014-12-08 10:25:01 -0500

    Started building db connection for api calls
    and defined schema

--- routes/courses.js
+++ routes/courses.js
@@ -7,6 +7,26 @@ var QUERIES = [
   "location", "size", "rating"
 ]
 
+var mongoose = require('mongoose');
+var Schema = mongoose.schema;
+var courseSchema = new Schema{
+  code: String
+  name: String
+  description: String
+  division: String
+  prerequisite: String
+  exlusions String
+  course_level: String
+  breadth: Array
+  campus: String
+  term: String
+  APSC_elec: String
+  meeting_sections: Array
+}
+var course = new mongoose.model("Course", schema)
+
+mongoose.connect('put data base here');
+
 router.get('/', function(req, res) {
 
   var query = req.query
@@ -20,15 +40,23 @@ router.get('/', function(req, res) {
     keys.push(key)
   }
 
+  var response = [];
+
   if (!clean) {
-    res.send(400)
+    res.send("yo")
   } else {
     for (var i = 0; i < keys.length; i++) {
       var key = keys[i].toLowerCase()
       var quer = query[key]
+      if(quer = "_id"){
+        response.push(getIdQuery())
+      }
     }
   }
 
 })
+
+function getIdQuery() = {
+}
 
 module.exports = router
