642f0ae01d63a3256e9f73c772c2667e4a8eb04f
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2014-12-08 13:59:09 -0500

    fixed some syntax

--- routes/courses.js
+++ routes/courses.js
@@ -8,24 +8,24 @@ var QUERIES = [
 ]
 
 var mongoose = require('mongoose');
-var Schema = mongoose.schema;
-var courseSchema = new Schema{
-  code: String
-  name: String
-  description: String
-  division: String
-  prerequisite: String
-  exlusions String
-  course_level: String
-  breadth: Array
-  campus: String
-  term: String
-  APSC_elec: String
+mongoose.connect('process.env.MONGOLAB_URL');
+var courseSchema = new mongoose.Schema({
+  course_id: String,
+  code: String,
+  name: String,
+  description: String,
+  division: String,
+  prerequisites: String,
+  exlusions: String,
+  course_level: String,
+  breadth: Array,
+  campus: String,
+  term: String,
+  apsc_elec: String,
   meeting_sections: Array
-}
-var course = new mongoose.model("Course", schema)
+})
+var courses = new mongoose.model("courses", courseSchema)
 
-mongoose.connect('put data base here');
 
 router.get('/', function(req, res) {
 
@@ -48,15 +48,18 @@ router.get('/', function(req, res) {
     for (var i = 0; i < keys.length; i++) {
       var key = keys[i].toLowerCase()
       var quer = query[key]
-      if(quer = "_id"){
-        response.push(getIdQuery())
+      if (key = "_id") {
+        response.push(getIdQuery(quer))
       }
     }
   }
 
 })
 
-function getIdQuery() = {
+function getIdQuery(key, quer) {
+  return courses.find({
+    key: quer
+  })
 }
 
 module.exports = router
