8791526d53497e616bbe21ec25afe0bd0317057a
Qasim Iqbal <me@qas.im>
2015-04-03 03:41:45 -0400

    Misc.

---
+++ routes/login.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var passport = require('passport')
+var User = require('../models/user')
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  if(req.user) {
+    res.redirect('/dashboard')
+  } else {
+    res.render('pages/login', { user: req.user })
+  }
+});
+
+router.post('/', passport.authenticate('local'), function(req, res) {
+  res.redirect('/dashboard');
+});
+
+
+module.exports = router;
