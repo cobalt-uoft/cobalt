d2fd21f1f1f56d0b6ad26efee6e89e700897bb11
Qasim Iqbal <me@qas.im>
2015-04-10 22:32:35 -0400

    +1 build version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('pages/index', { user: req.user, build: '0.0a5' })
+  res.render('pages/index', { user: req.user, build: '0.0a6' })
 })
 
 module.exports = router
