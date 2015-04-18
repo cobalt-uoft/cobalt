6f83473979c410d75eab5cd9e485c2edaa42d1cd
Qasim Iqbal <me@qas.im>
2015-04-18 02:02:16 -0400

    Remove submodule

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ var user = {
 	login: require('./user/routes/login'),
 	logout: require('./user/routes/logout'),
 	signup: require('./user/routes/signup'),
+	verify: require('./user/routes/verify')
 	dashboard: require('./user/routes/dashboard')
 }
 
@@ -90,6 +91,7 @@ app.use('/docs', docs)
 app.use('/login', user.login)
 app.use('/logout', user.logout)
 app.use('/signup', user.signup)
+app.user('/verify', user.verify)
 app.use('/dashboard', user.dashboard)
 
 /* API routes */
