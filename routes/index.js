65093e3c01603bb180a83b46dd34088b6c36993c
Qasim Iqbal <me@qas.im>
2015-04-22 22:39:58 -0400

    Design modifications

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res) {
-  res.render('pages/index', { user: req.user, build: '0.0a11' })
+  res.render('pages/index', { user: req.user, build: '0.0a12' })
 })
 
 module.exports = router
