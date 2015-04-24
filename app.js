cb7c135968b2077980227e888a03f2e3847a466a
Spencer Elliott <me@elliottsj.com>
2015-04-23 20:48:12 -0400

    Use arrows & export default

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use('/api/food', uoftFoodApi)
 /* Error handlers */
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   let err = new Error('Not Found')
   err.status = 404
   next(err)
@@ -71,10 +71,10 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500)
     res.json({
-      "error": {
+      error: {
         code: err.status,
         message: err.message
       }
@@ -84,7 +84,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500)
   res.render('error', {
     message: err.message,
@@ -92,5 +92,4 @@ app.use(function(err, req, res, next) {
   })
 })
 
-
-module.exports = app
+export default app
