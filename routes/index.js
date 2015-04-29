1c042ae94c4d7f088a31dc3394c1dc9afeb72f16
Qasim Iqbal <me@qas.im>
2015-04-28 21:13:11 -0400

    Update page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 let router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('pages/index', { user: req.user, build: '0.0a12' })
+  res.render('pages/index', { user: req.user, build: '0.0a13' })
 })
 
 export default router
