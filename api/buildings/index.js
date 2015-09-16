cee3d350a52996dc15a18617abb8bcc5ee0fb632
Qasim Iqbal <me@qas.im>
2015-09-16 01:09:24 -0400

    Fix search buildings

--- api/buildings/index.js
+++ api/buildings/index.js
@@ -3,7 +3,7 @@ let router = express.Router()
 
 import list from './routes/list'
 import show from './routes/show'
-import search from './routes/show'
+import search from './routes/search'
 
 router.get('/list', list)
 router.get('/show/:id', show)
