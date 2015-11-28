0c1c5ac3a8e98326d5ec5f919cbb9fee15f01ada
Qasim Iqbal <me@qas.im>
2015-11-28 01:27:01 -0500

    Add empty filter endpoint

--- api/buildings/index.js
+++ api/buildings/index.js
@@ -4,9 +4,11 @@ let router = express.Router()
 import list from './routes/list'
 import show from './routes/show'
 import search from './routes/search'
+import filter from './routes/filter'
 
 router.get('/list', list)
 router.get('/show/:id', show)
 router.get('/search', search)
+router.get('/filter', filter)
 
 export default router
---
+++ api/buildings/routes/filter.js
@@ -0,0 +1,41 @@
+import Building from '../model'
+import co from 'co'
+
+// Default values
+let limit = 10
+let skip = 0
+let sort = 'id'
+
+export default function get(req, res, next) {
+  let qLimit = limit
+  if (req.query.limit) {
+    if (isNaN(req.query.limit) || req.query.limit < 1 || req.query.limit > 100) {
+      let err = new Error('Limit must be a positive integer greater than 1 and less than or equal to 100.')
+      err.status = 400
+      return next(err)
+    }
+    qLimit = req.query.limit
+  }
+
+  let qSkip = skip
+  if (req.query.skip) {
+    if (isNaN(req.query.skip) || req.query.skip < 0) {
+      let err = new Error('Skip must be a positive integer.')
+      err.status = 400
+      return next(err)
+    }
+    qSkip = req.query.skip
+  }
+
+  let qSort = sort
+  if (req.query.sort) {
+    if (req.query.sort.length < 2) {
+      let err = new Error('Sort must be a string of length greater than 1.')
+      err.status = 400
+      return next(err)
+    }
+    qSort = req.query.sort
+  }
+
+  res.json({})
+}
