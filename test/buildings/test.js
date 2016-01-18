172e3bc561b069ef4ed6c382cc3133e2db10659a
Qasim Iqbal <me@qas.im>
2016-01-17 23:12:38 -0500

    Implement building tests

--- test/buildings/test.js
+++ test/buildings/test.js
@@ -17,12 +17,172 @@ test.before('setup', t => {
   })
 })
 
+/* list tests */
+
 test('/', t => {
   request(app)
     .get('/1.0/buildings')
     .expect('Content-Type', /json/)
     .expect(200)
-    .end(function(err, res){
+    .expect(JSON.stringify(testData.slice(0, 10)))
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+test('/?limit=0', t => {
+  request(app)
+    .get('/1.0/buildings?limit=0')
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+test('/?limit=2', t => {
+  request(app)
+    .get('/1.0/buildings?limit=2')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect(JSON.stringify(testData.slice(0, 2)))
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+test('/?limit=200', t => {
+  request(app)
+    .get('/1.0/buildings?limit=200')
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+test('/?skip=10', t => {
+  request(app)
+    .get('/1.0/buildings?skip=10')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect(JSON.stringify(testData.slice(10, 20)))
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+test('/?skip=200', t => {
+  request(app)
+    .get('/1.0/buildings?skip=200')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect('[]')
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+test('/?skip=2&limit=2', t => {
+  request(app)
+    .get('/1.0/buildings?skip=2&limit=2')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect(JSON.stringify(testData.slice(2, 4)))
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+/* show tests */
+
+test(`/${testData[0].id}`, t => {
+  request(app)
+    .get(`/1.0/buildings/${testData[0].id}`)
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect(JSON.stringify(testData[0]))
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+test('/XYZ', t => {
+  request(app)
+    .get('/1.0/buildings/XYZ')
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+/* search tests */
+
+test('/search?q=', t => {
+  request(app)
+    .get('/1.0/buildings/search?q=')
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+test('/search?q=Wallberg', t => {
+  request(app)
+    .get('/1.0/buildings/search?q=Wallberg')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect(JSON.stringify(testData.filter(doc => { return doc.name.match('Wallberg') })))
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+test('/search?q=loremipsumdolorsitamet', t => {
+  request(app)
+    .get('/1.0/buildings/search?q=loremipsumdolorsitamet')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect('[]')
+    .end((err, res) => {
+      if (err) t.fail(err.message)
+      t.pass()
+      t.end()
+    })
+})
+
+/* TODO: filter tests */
+
+test('/filter?q=', t => {
+  request(app)
+    .get('/1.0/buildings/filter?q=')
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .end((err, res) => {
       if (err) t.fail(err.message)
       t.pass()
       t.end()
@@ -30,9 +190,8 @@ test('/', t => {
 })
 
 test.after('cleanup', t => {
-  /*Building.remove({}, err => {
+  Building.remove({}, err => {
     if (err) t.fail(err.message)
     t.end()
-  })*/
-  t.end()
+  })
 })
