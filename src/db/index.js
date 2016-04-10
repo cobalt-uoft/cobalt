8978f4965c518a5063582abfec34f057dac8799e
Qasim Iqbal <me@qas.im>
2016-04-10 10:52:44 -0400

    Fix tabbing and incorrect variable reference

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,14 @@
-import https from 'https'
-import winston from 'winston'
-import fs from 'fs'
-import schedule from 'node-schedule'
 import childProcess from 'child_process'
+import fs from 'fs'
+import https from 'https'
 import mongoose from 'mongoose'
+import schedule from 'node-schedule'
+import winston from 'winston'
+
+// Holds the last commit that was synchronized
+let lastCommit  =  ''
 
 let db = {}
-//I'm not sure why you guys use let 
-let lastCommit	=	'';
 
 db.update = (collection) => {
   let url = `https://raw.githubusercontent.com/cobalt-uoft/datasets/master/${collection}.json`
@@ -45,7 +46,8 @@ db.update = (collection) => {
     })
 
   }).on('error', e => {
-    winston.warn('Could not update database, online datasets are currently inaccessible.', e)
+    winston.warn('Could not update database, online datasets are \
+      currently inaccessible.', e)
   })
 }
 
@@ -57,37 +59,32 @@ db.sync = () => {
 }
 
 db.check = (callback) => {
-	
-	let options	=	{
-		host: `api.github.com`,
-		port: 443,
-		path:`/repos/cobalt-uoft/datasets/git/refs/heads/master`,
-		headers: {'user-agent': 'cobalt-selfhost-user'}
-	}
-	
-	https.get(options, res	=> {
-		let data	=	""
+  let options  =  {
+    host: 'api.github.com',
+    port: 443,
+    path: '/repos/cobalt-uoft/datasets/git/refs/heads/master',
+  }
 
-		res.on('data', chunk => {
-			data+=chunk
-		})
+  https.get(options, res  => {
+    let data = ''
 
-		res.on('end', () => {
-			data	=	JSON.parse(data)
-			
-			//Compare the last commit hash to the current one
-			if(data.object.sha==lastCommit)
-				 return;
+    res.on('data', chunk => {
+      data += chunk
+    })
 
-			db.lastCommit = data.object.sha;
-			
-			//Execute the callback
-			callback&&callback();
-		})
-	})
+    res.on('end', () => {
+      data = JSON.parse(data)
 
+      // Compare the last commit hash to the current one
+      if (data.object.sha == lastCommit) return
+
+      lastCommit = data.object.sha
+
+      // Execute the callback
+      if (callback) callback()
+    })
+  })
 }
-
 
 db.syncCron = () => {
   // Make data directory if it doesn't exist
@@ -97,10 +94,12 @@ db.syncCron = () => {
     fs.mkdirSync('.cobalt_data')
   }
 
+  // Perform sync on startup
   db.sync()
-	//Cool we can make this more accurate
-  schedule.scheduleJob('30 1 * * *', () => {
-		db.check(()=>db.sync())
+
+  // Schedule checking for sync every hour
+  schedule.scheduleJob('0 * * * *', () => {
+    db.check(() => { db.sync() })
   })
 }
 
