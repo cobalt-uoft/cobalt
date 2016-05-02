895d086675247d010f35bdb1ad9cecb47b3d2fda
Qasim Iqbal <me@qas.im>
2016-05-01 21:01:52 -0400

    Use new filter method in buildings

--- src/api/buildings/routes/filter.js
+++ src/api/buildings/routes/filter.js
@@ -1,147 +1,60 @@
 import Building from '../model'
 import co from 'co'
+import QueryParser from '../../utils/query-parser'
 
-// The absolute (from root) keymap
-const ABSOLUTE_KEYMAP = {
-  'code': 'code',
-  'name': 'name',
-  'short_name': 'short_name',
-  'campus': 'campus',
-  'street': 'address.street',
-  'city': 'address.city',
-  'country': 'address.country',
-  'postal': 'address.postal',
-  'lat': 'lat',
-  'lng': 'lng',
-  'polygon': 'polygon'
+const KEYMAP = {
+  'code':       { type: QueryParser.String, value: 'code' },
+  'name':       { type: QueryParser.String, value: 'name' },
+  'short_name': { type: QueryParser.String, value: 'short_name' },
+  'campus':     { type: QueryParser.String, value: 'campus' },
+  'street':     { type: QueryParser.String, value: 'address.street' },
+  'city':       { type: QueryParser.String, value: 'address.city' },
+  'country':    { type: QueryParser.String, value: 'address.country' },
+  'postal':     { type: QueryParser.String, value: 'address.postal' },
+  'lat':        { type: QueryParser.Number, value: 'lat' },
+  'lng':        { type: QueryParser.Number, value: 'lng' }
 }
 
 export default function filter(req, res, next) {
-  let q = req.query.q
-  q = q.split(' AND ')
 
-  let queries = 0
+  // Split query into tokens
+  let q = QueryParser.tokenize(req.query.q)
 
-  let filter = { $and: q }
-
-  for (let i = 0; i < filter.$and.length; i++) {
-    filter.$and[i] = { $or: q[i].trim().split(' OR ') }
-
-    for (let j = 0; j < filter.$and[i].$or.length; j++) {
-      let part = filter.$and[i].$or[j].trim().split(':')
-      let x = formatPart(part[0], part[1])
-
-      if (x.isValid) {
-        filter.$and[i].$or[j] = x.query
-        queries++
+  // Parse each token
+  for (let i = 0; i < q.length; i++) {
+    for (let j = 0; j < q[i].length; j++) {
+      q[i][j] = QueryParser.parse(q[i][j])
+      if (!KEYMAP.hasOwnProperty(q[i][j].key)) {
+        let err = new Error('At least one provided filter key is not supported.')
+        err.status = 400
+        return next(err)
       }
     }
   }
 
-  if(queries > 0) {
-    co(function* () {
-      try {
-        let docs = yield Building
-          .find(filter, '-__v -_id -address._id')
-          .limit(req.query.limit)
-          .skip(req.query.skip)
-          .sort(req.query.sort)
-          .exec()
-        res.json(docs)
-      } catch(e) {
-        return next(e)
-      }
-    })
-  }
-
-}
-
-function formatPart(key, part) {
-  // Response format
-  let response = {
-    key: key,
-    isValid: true,
-    query: {}
-  }
-
-  // Checking if the start of the segment is an operator (-, >, <, .>, .<)
-  if (part.indexOf('-') === 0) {
-    // Negation
-    part = {
-      operator: '-',
-      value: part.substring(1)
-    }
-  } else if (part.indexOf('>=') === 0) {
-    part = {
-      operator: '>=',
-      value: part.substring(2)
-    }
-  } else if (part.indexOf('<=') === 0) {
-    part = {
-      operator: '<=',
-      value: part.substring(2)
-    }
-  } else if (part.indexOf('>') === 0) {
-    part = {
-      operator: '>',
-      value: part.substring(1)
-    }
-  } else if (part.indexOf('<') === 0) {
-    part = {
-      operator: '<',
-      value: part.substring(1)
-    }
-  } else {
-    part = {
-      operator: undefined,
-      value: part
+  // Generate MongoDB-compatible filter
+  let filter = { $and: [] }
+  for (let i = 0; i < q.length; i++) {
+    filter.$and[i] = { $or: [] }
+    for (let j = 0; j < q[i].length; j++) {
+      let query = KEYMAP[q[i][j].key].type(q[i][j].filter)
+      filter.$and[i].$or[j] = {}
+      filter.$and[i].$or[j][KEYMAP[q[i][j].key].value] = query
     }
   }
 
-  if (isNaN(parseFloat(part.value)) || !isFinite(part.value)) {
-    // Is not a number
-    part.value = part.value.substring(1, part.value.length - 1)
-  } else {
-    part.value = parseFloat(part.value)
-    if (part.operator === '-') {
-      part.value = -part.value
+  co(function* () {
+    try {
+      let docs = yield Building
+        .find(filter, '-__v -_id -address._id')
+        .limit(req.query.limit)
+        .skip(req.query.skip)
+        .sort(req.query.sort)
+        .exec()
+      res.json(docs)
+    } catch(e) {
+      return next(e)
     }
-  }
+  })
 
-  if (['lat', 'lng'].indexOf(key) > -1) {
-    // Numbers and arrays of Numbers
-
-    if (part.operator === '>') {
-      response.query[ABSOLUTE_KEYMAP[key]] = { $gt: part.value }
-    } else if (part.operator === '<') {
-      response.query[ABSOLUTE_KEYMAP[key]] = { $lt: part.value }
-    } else if (part.operator === '>=') {
-      response.query[ABSOLUTE_KEYMAP[key]] = { $gte: part.value }
-    } else if (part.operator === '<=') {
-      response.query[ABSOLUTE_KEYMAP[key]] = { $lte: part.value }
-    } else {
-      // Assume equality if no operator
-      response.query[ABSOLUTE_KEYMAP[key]] = part.value
-    }
-  } else if (key === 'polygon') {
-    // TODO: Arrays of [Number, Number]
-
-  } else {
-    // Strings
-
-    if (part.operator === '-') {
-      response.query[ABSOLUTE_KEYMAP[key]] = {
-        $regex: '^((?!' + escapeRe(part.value) + ').)*$',
-        $options: 'i'
-      }
-    } else {
-      response.query[ABSOLUTE_KEYMAP[key]] = { $regex: '(?i).*' + escapeRe(part.value) + '.*' }
-    }
-  }
-
-  return response
-}
-
-function escapeRe(str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&')
 }
--- src/api/utils/query-parser/index.js
+++ src/api/utils/query-parser/index.js
@@ -1,29 +1,134 @@
 class QueryParser {
 
+  /*
+    Split query into individual tokens.
+  */
   static tokenize (query) {
+    let pieces = query.split(' AND ')
 
+    for (let i = 0; i < pieces.length; i++) {
+      pieces[i] = pieces[i].trim().split(' OR ')
+      for (let j = 0; j < pieces[i].length; j++) {
+        pieces[i][j] = pieces[i][j].trim()
+      }
+    }
+
+    return pieces
   }
 
+  /*
+    Parse token to retrieve key and filter information.
+  */
   static parse (token) {
+    let response = {}
+
+    if (token.indexOf(':') < 0) {
+      response.error = new Error('Missing separator between key and filter.')
+      response.error.status = 400
+      return response
+    }
+
+    response = {
+      raw: token,
+      key: token.slice(0, token.indexOf(':')).trim(),
+      filter: token.slice(token.indexOf(':') + 1).trim()
+    }
+
+    if (response.filter.indexOf('-') === 0) {
+      // Negation
+      response.filter = {
+        operator: '-',
+        value: response.filter.substring(1)
+      }
+    } else if (response.filter.indexOf('>=') === 0) {
+      response.filter = {
+        operator: '>=',
+        value: response.filter.substring(2)
+      }
+    } else if (response.filter.indexOf('<=') === 0) {
+      response.filter = {
+        operator: '<=',
+        value: response.filter.substring(2)
+      }
+    } else if (response.filter.indexOf('>') === 0) {
+      response.filter = {
+        operator: '>',
+        value: response.filter.substring(1)
+      }
+    } else if (response.filter.indexOf('<') === 0) {
+      response.filter = {
+        operator: '<',
+        value: response.filter.substring(1)
+      }
+    } else {
+      response.filter = {
+        operator: undefined,
+        value: response.filter
+      }
+    }
+
+    if (isNaN(parseFloat(response.filter.value)) || !isFinite(response.filter.value)) {
+      // Is not a number
+      response.filter.value = response.filter.value.substring(1,
+        response.filter.value.length - 1)
+    } else {
+      response.filter.value = parseFloat(response.filter.value)
+    }
+
+    return response
+  }
+
+  /*
+    Form the MongoDB-compatible filter for strings.
+  */
+  static String (filter) {
+    if (filter.operator === '-') {
+      return {
+        $regex: '^((?!' + escapeRe(filter.value) + ').)*$',
+        $options: 'i'
+      }
+    }
+    return { $regex: '(?i).*' + escapeRe(filter.value) + '.*' }
+  }
+
+  /*
+    Form the MongoDB-compatible filter for numbers.
+  */
+  static Number (filter) {
+    if (filter.operator === '>') {
+      return { $gt: filter.value }
+    } else if (filter.operator === '<') {
+      return { $lt: filter.value }
+    } else if (filter.operator === '>=') {
+      return { $gte: filter.value }
+    } else if (filter.operator === '<=') {
+      return { $lte: filter.value }
+    } else if (filter.operator === '-') {
+      return -filter.value
+    } else {
+      // Assume equality if no operator
+      return filter.value
+    }
+  }
+
+  /*
+    Form the MongoDB-compatible filter for dates.
+  */
+  static Date (value) {
 
   }
 
-  static parseString (value) {
+  /*
+    Form the MongoDB-compatible filter for times.
+  */
+  static Time (value) {
 
   }
 
-  static parseNumber (value) {
+}
 
-  }
-
-  static parseDate (value) {
-
-  }
-
-  static parseTime (value) {
-
-  }
-
+function escapeRe (str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&')
 }
 
 export default QueryParser
