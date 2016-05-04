5705ed4ca86407fc7dfac04f6f9c6dc905c584eb
Qasim Iqbal <me@qas.im>
2016-05-04 13:40:03 -0400

    Remove unused imports

--- src/api/cdf/printers/index.js
+++ src/api/cdf/printers/index.js
@@ -2,9 +2,6 @@ import express from 'express'
 let router = express.Router()
 
 import list from './routes/list'
-
-import Validator from '../../utils/validator'
-
 router.get('/', list)
 
 export default router
